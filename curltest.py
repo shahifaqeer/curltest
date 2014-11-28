097236158e506d4c0cae2459bccd52ce73c38f04
Sarthak Grover <shahifaqeer@gmail.com>
2014-11-28 08:32:23 -0500

    print current time along with DONE

--- curltest.py
+++ curltest.py
@@ -1,4 +1,5 @@
 import subprocess
+from random import shuffle
 from classes import Router
 import time
 
@@ -8,8 +9,10 @@ def download(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
     cmd = "curl 10.0.0.1:8055/test" + file_zize + ".gz -o /dev/null --stderr /home/gtnoise/test/"+outfile
     if proxy == 1:
         cmd += " -x http://10.0.2.1:3128"
+    if proxy == 2:
+        cmd += " -x http://10.0.0.1:3128"
     # BLOCKING CMD
-    subprocess.check_output(cmd, shell=True)
+    o = subprocess.check_output(cmd, shell=True)
     return 0
 
 def download_DEBUG(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
@@ -17,7 +20,11 @@ def download_DEBUG(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
     cmd = "curl 10.0.0.1:8055/test" + file_zize + ".gz -o /dev/null"
     if proxy == 1:
         cmd += " -x http://10.0.2.1:3128"
+    if proxy == 2:
+        cmd += " -x http://10.0.0.1:3128"
     # BLOCKING CMD
+    print cmd
+    print "DELAY ", delay, " Run Number ", str(run_num), " PROXY ", str(proxy)
     o = subprocess.check_output(cmd, shell=True)
     print o
     return
@@ -38,42 +45,41 @@ def set_rate_delay(Q, rate, delay):
             Q.remoteCommand('tc qdisc del dev br-lan root')
     return 0
 
-def test_all_combos(Q, M):
-    for rate in [0, 5, 10, 15, 20, 25]:
-        for file_size in ['500K', '2M', '10M']:
+def test_all_combos(Q, M, S, file_size='500K'):
+    proxies = range(3)
+    for delay in [0, 5, 10, 15, 20, 25, 30, 35, 40, 60, 80, 100]:
+        for rate in [0, 5, 10, 15, 20, 25, 30, 35, 40, 60, 80, 100]:
             M.remoteCommand('sudo sh clearcache.sh')
-            for run_num in range(20):
-                for delay in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
-                    set_rate_delay(Q, rate, delay)
-                    time.sleep(0.1)
-                    print "DONE delay "+str(delay)+ " Run number "+str(run_num)+" file_size " + file_size
-                    for proxy in [0, 1]:
-                        download(proxy, file_size, run_num, str(rate), str(delay))
-                        if proxy == 1:
-                            M.remoteCommand('sudo sh clearcache.sh')
+            S.remoteCommand('sudo sh clearcache.sh')
+            set_rate_delay(Q, rate, delay)
             time.sleep(1)
+            for run_num in range(50):
+                time.sleep(0.1)
+                shuffle(proxies)
+                for proxy in proxies:
+                    download(proxy, file_size, run_num, str(rate), str(delay))
+                    clear_polipo_cache(M, S)
+            print time.time(), ": DONE rate "+str(rate)+" delay "+str(delay)+ " file_size " + file_size
     return
 
-def clear_polipo_cache(M):
+def clear_polipo_cache(M, S):
     M.remoteCommand('sudo sh clearcache.sh')
+    S.remoteCommand('sudo sh clearcache.sh')
     return
 
-def quick_test(Q, M):
+def quick_test(Q, M, S):
     file_size = '500K'
-    for run_num in range(5):
-        for delay in [1, 10, 30, 100]:
-            set_rate_delay(Q, 1, delay)
+    rate = 5
+    proxies = range(3)
+    for delay in [1, 10, 20, 30, 100]:
+        for run_num in range(5):
+            clear_polipo_cache(M, S)
+            set_rate_delay(Q, rate, delay)
             time.sleep(0.1)
-            print "DONE rate, delay "+str(1)+", "+str(delay)+ " Run number "+str(run_num)+" file_size " + file_size
-            for proxy in [0, 1]:
-                download_DEBUG(proxy, file_size, run_num, '0', str(delay))
-                if proxy == 0:
-                    print "clear cache"
-                    M.remoteCommand('sudo kill -USR1 $(pgrep polipo)')
-                    M.remoteCommand('sleep 0.1')
-                    M.remoteCommand('sudo polipo -x')
-                    M.remoteCommand('sudo kill -USR2 $(pgrep polipo)')
-                    #clear_polipo_cache(M)
+            shuffle(proxies)
+            for proxy in proxies:
+                download_DEBUG(proxy, file_size, run_num, str(rate), str(delay))
+                clear_polipo_cache(M, S)
     return
 
 
@@ -81,13 +87,22 @@ if __name__=="__main__":
 
     #R = Router('192.168.10.1', 'root', 'passw0rd', 'R')
     Q = Router('10.0.1.1', 'root', 'passw0rd', 'Q')
-    M = Router('10.0.2.1', 'sarthak', 'sarthak123', 'Q')
+    M = Router('10.0.2.1', 'sarthak', 'sarthak123', 'M')
+    S = Router('10.0.0.1', 'gtnoise', 'gtnoise', 'S')
 
-    #quick_test(Q)
     start_t = time.time()
+    print "START ", start_t
 
-    test_all_combos(Q, M)
+    #quick_test(Q, M, S)
+    test_all_combos(Q, M, S, '2M')
 
+    set_rate_delay(Q, 0, 0)
+    done_t = time.time()
+    print "DONE ", (done_t - start_t)
+
+    start_t = time.time()
+    print "START ", start_t
+    test_all_combos(Q, M, S, '10M')
     set_rate_delay(Q, 0, 0)
     done_t = time.time()
     print "DONE ", (done_t - start_t)
@@ -108,3 +123,4 @@ if __name__=="__main__":
     '''
     Q.host.close()
     M.host.close()
+    S.host.close()
