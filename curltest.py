077f13df9b37994067bb412b4eba10976588a801
Sarthak Grover <shahifaqeer@gmail.com>
2014-12-09 05:52:32 -0500

    tcpdump with curl for bad/good access

--- curltest.py
+++ curltest.py
@@ -4,9 +4,9 @@ from classes import Router
 import time
 
 
-def download(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
-    outfile = "curl-proxy_"+str(proxy)+"-file_"+file_zize+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+".log"
-    cmd = "curl 10.0.0.1:8055/test" + file_zize + ".gz -o /dev/null --stderr /home/gtnoise/test/"+outfile
+def download(proxy=1, file_size='10M', run_num=0, rate='', delay=''):
+    outfile = "curl-proxy_"+str(proxy)+"-file_"+file_size+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+".log"
+    cmd = "curl 10.0.0.1:8055/test" + file_size + ".gz -o /dev/null --stderr /home/gtnoise/test/"+outfile
     if proxy == 1:
         cmd += " -x http://10.0.2.1:3128"
     if proxy == 2:
@@ -15,9 +15,9 @@ def download(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
     o = subprocess.check_output(cmd, shell=True)
     return 0
 
-def download_DEBUG(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
-    outfile = "curl-proxy_"+str(proxy)+"-file_"+file_zize+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+".log"
-    cmd = "curl 10.0.0.1:8055/test" + file_zize + ".gz -o /dev/null"
+def download_DEBUG(proxy=1, file_size='10M', run_num=0, rate='', delay=''):
+    outfile = "curl-proxy_"+str(proxy)+"-file_"+file_size+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+".log"
+    cmd = "curl 10.0.0.1:8055/test" + file_size + ".gz -o /dev/null"
     if proxy == 1:
         cmd += " -x http://10.0.2.1:3128"
     if proxy == 2:
@@ -29,20 +29,39 @@ def download_DEBUG(proxy=1, file_zize='10M', run_num=0, rate='', delay=''):
     print o
     return
 
+def tcpdump(M, proxy, file_size, run_num, rate, delay):
+    # M
+    outfile = "tcpdump-proxy_"+str(proxy)+"-file_"+file_size+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+"_M.pcap"
+    cmd = "sudo tcpdump -i eth1 -s 300 -n -p -U -w /home/sarthak/test/"+outfile
+    M.remoteCommand(cmd)
+    # client
+    outfile = "tcpdump-proxy_"+str(proxy)+"-file_"+file_size+"-rate_"+rate+"-delay_"+delay+"-run_"+str(run_num)+"_A.pcap"
+    cmd = "sudo tcpdump -i wlan0mon -s 300 -n -p -U -w /home/gtnoise/test/"+outfile+" &"
+    subprocess.call(cmd, shell=True)
+    return
+
+def killtcpdump(M):
+    cmd = "sudo killall tcpdump"
+    M.remoteCommand(cmd)
+    subprocess.check_output(cmd, shell=True)
+    return
+
+def transferdump(M):
+    cmd = "scp -r sarthak@10.0.2.1:test/* /home/gtnoise/test/"
+    subprocess.check_output(cmd, shell=True)
+    M.remoteCommand("sudo rm -f /home/sarthak/test/*.pcap")
+    return
+
 def set_rate_delay(Q, rate, delay):
     rate_bits = str(rate)
     delay_ms = str(delay)
-    Q.remoteCommand('tc qdisc del dev br-lan root')
+    Q.remoteCommand('tc qdisc del dev br-lan root;tc qdisc del dev eth0 root;tc qdisc del dev eth1 root')
     if rate != 0:
-        Q.remoteCommand('sh ratelimit4.sh eth0 '+rate_bits+' '+delay_ms)
-        Q.remoteCommand('sh ratelimit4.sh eth1 '+rate_bits+' '+delay_ms)
+        Q.remoteCommand('sh ratelimit5.sh br-lan '+rate_bits+' '+delay_ms)
+        Q.remoteCommand('sh ratelimit5.sh eth1 '+rate_bits+' '+delay_ms)
     else:
-        Q.remoteCommand('tc qdisc del dev eth0 root')
+        Q.remoteCommand('tc qdisc del dev br-lan root')
         Q.remoteCommand('tc qdisc del dev eth1 root')
-        if delay > 0:
-            Q.remoteCommand('tc qdisc add dev br-lan root netem delay' +delay_ms+ 'ms')
-        else:
-            Q.remoteCommand('tc qdisc del dev br-lan root')
     return 0
 
 def test_all_combos(Q, M, S, file_size='500K'):
@@ -61,6 +80,33 @@ def test_all_combos(Q, M, S, file_size='500K'):
                     clear_polipo_cache(M, S)
             print time.time(), ": DONE rate "+str(rate)+" delay "+str(delay)+ " file_size " + file_size
     return
+
+def test_access(Q, M, S):
+    for run_num in range(20):
+        for access in ['good', 'bad']:
+            #good access
+            if access == 'good':
+                rate = 100
+                delay = 5
+            else:
+                rate = 10
+                delay = 50
+            set_rate_delay(Q, rate, delay)
+
+            for file_size in ['500K', '10M']:
+                M.remoteCommand('sudo sh clearcache.sh')
+                S.remoteCommand('sudo sh clearcache.sh')
+
+                proxies = range(3)
+                shuffle(proxies)
+                for proxy in proxies:
+                    tcpdump(M, proxy, file_size, run_num, str(rate), str(delay))
+                    time.sleep(0.1)
+                    download(proxy, file_size, run_num, str(rate), str(delay))
+                    clear_polipo_cache(M, S)
+                    killtcpdump(M)
+        print time.time(), ": DONE run_num "+str(run_num)
+        return
 
 def clear_polipo_cache(M, S):
     M.remoteCommand('sudo sh clearcache.sh')
@@ -82,30 +128,31 @@ def quick_test(Q, M, S):
                 clear_polipo_cache(M, S)
     return
 
-
-if __name__=="__main__":
-
-    #R = Router('192.168.10.1', 'root', 'passw0rd', 'R')
+def setup_remote():
     Q = Router('10.0.1.1', 'root', 'passw0rd', 'Q')
     M = Router('10.0.2.1', 'sarthak', 'sarthak123', 'M')
     S = Router('10.0.0.1', 'gtnoise', 'gtnoise', 'S')
+    return Q, M, S
+
+
+if __name__=="__main__":
+
+    Q, M, S = setup_remote()
 
     start_t = time.time()
     print "START ", start_t
 
     #quick_test(Q, M, S)
-    test_all_combos(Q, M, S, '2M')
+    #test_all_combos(Q, M, S, '2M')
+    test_access(Q,M,S)
 
-    set_rate_delay(Q, 0, 0)
     done_t = time.time()
     print "DONE ", (done_t - start_t)
 
-    start_t = time.time()
-    print "START ", start_t
-    test_all_combos(Q, M, S, '10M')
+    print "TRANSFER"
     set_rate_delay(Q, 0, 0)
-    done_t = time.time()
-    print "DONE ", (done_t - start_t)
+    transferdump(M)
+
     #quick_test(Q, M)
     '''
     set_rate_delay(Q, 1, 100)
